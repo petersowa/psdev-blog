14335863534f064d36ef62531357a405fffd2f9c
petersowa <petersowa@users.noreply.github.com>
2020-01-28 09:07:45 -0600

    blog page with graphql query for remark data

---
+++ src/pages/blog.js
@@ -0,0 +1,48 @@
+import React from "react"
+import { graphql } from "gatsby"
+import { Link } from "gatsby"
+// import { css } from "@emotion/core"
+// import { rhythm } from "../utils/typography"
+import Layout from "../components/layout"
+
+export default ({ data }) => {
+  console.log(data)
+  return (
+    <Layout>
+      <div>
+        <h1>Amazing Pandas Eating Things</h1>
+        <h4>{data.allMarkdownRemark.totalCount} Posts</h4>
+        {data.allMarkdownRemark.edges.map(({ node }) => (
+          <div key={node.id}>
+            <h3>
+              {node.frontmatter.title} <span>— {node.frontmatter.date}</span>
+            </h3>
+            <p>{node.excerpt}</p>
+            <Link to={node.frontmatter.path}>See more...</Link>
+            {/* <div dangerouslySetInnerHTML={{ __html: node.html }} /> */}
+          </div>
+        ))}
+      </div>
+    </Layout>
+  )
+}
+
+export const query = graphql`
+  query {
+    allMarkdownRemark {
+      totalCount
+      edges {
+        node {
+          id
+          frontmatter {
+            title
+            date(formatString: "DD MMMM, YYYY")
+            path
+          }
+          excerpt
+          html
+        }
+      }
+    }
+  }
+`
