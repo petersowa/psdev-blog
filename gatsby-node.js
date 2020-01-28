4ba5c2465b7d82125ece28d7d82001c3a679c906
petersowa <petersowa@users.noreply.github.com>
2020-01-28 09:04:41 -0600

    add dynamic remark page handling

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,3 +5,38 @@
  */
 
 // You can delete this file if you're not using it
+
+const path = require("path")
+
+exports.onCreateNode = ({ node }) => {
+  if (node.internal.type === "MarkdownRemark") {
+    console.log("---", node.internal.type)
+  }
+}
+
+exports.createPages = async ({ actions, graphql }) => {
+  const { createPage } = actions
+  const result = await graphql(`
+    query {
+      allMarkdownRemark {
+        edges {
+          node {
+            frontmatter {
+              path
+            }
+          }
+        }
+      }
+    }
+  `)
+
+  result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+    createPage({
+      path: node.frontmatter.path,
+      component: path.resolve("./src/templates/post-view.js"),
+      context: {
+        slug: node.frontmatter.path,
+      },
+    })
+  })
+}
