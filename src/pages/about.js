d03b3a4d49c735573aa0c0fd65d81c05bc197724
petersowa <petersowa@users.noreply.github.com>
2020-01-30 10:42:22 -0600

    update content and layout

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,15 +1,17 @@
 import React from 'react';
-import { Link } from 'gatsby';
 
 import Layout from '../components/layout';
 import SEO from '../components/seo';
 
 const SecondPage = () => (
     <Layout>
-        <SEO title="Page two" />
-        <h1>Hi from the second page</h1>
-        <p>Welcome to page 2</p>
-        <Link to="/">Go back to the homepage</Link>
+        <SEO title="About" />
+        <h1>About Page</h1>
+        <p className="article-columns">
+            I am a web developer. Working on mostly on front end UI/UX dev. I
+            also have exprience working with back end dev tools such as
+            Express.js.
+        </p>
     </Layout>
 );
 
--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,48 +1,51 @@
-import React from "react"
-import { graphql } from "gatsby"
-import { Link } from "gatsby"
+import React from 'react';
+import { graphql } from 'gatsby';
+import { Link } from 'gatsby';
+import SEO from '../components/seo';
 // import { css } from "@emotion/core"
 // import { rhythm } from "../utils/typography"
-import Layout from "../components/layout"
+import Layout from '../components/layout';
 
 export default ({ data }) => {
-  console.log(data)
-  return (
-    <Layout>
-      <div>
-        <h1>Amazing Pandas Eating Things</h1>
-        <h4>{data.allMarkdownRemark.totalCount} Posts</h4>
-        {data.allMarkdownRemark.edges.map(({ node }) => (
-          <div key={node.id}>
-            <h3>
-              {node.frontmatter.title} <span>— {node.frontmatter.date}</span>
-            </h3>
-            <p>{node.excerpt}</p>
-            <Link to={node.frontmatter.path}>See more...</Link>
-            {/* <div dangerouslySetInnerHTML={{ __html: node.html }} /> */}
-          </div>
-        ))}
-      </div>
-    </Layout>
-  )
-}
+    console.log(data);
+    return (
+        <Layout>
+            <SEO title="Blog" />
+            <div>
+                <h1>Amazing Pandas Eating Things</h1>
+                <h4>{data.allMarkdownRemark.totalCount} Posts</h4>
+                {data.allMarkdownRemark.edges.map(({ node }) => (
+                    <div key={node.id}>
+                        <h3>
+                            {node.frontmatter.title}{' '}
+                            <span>— {node.frontmatter.date}</span>
+                        </h3>
+                        <p>{node.excerpt}</p>
+                        <Link to={node.frontmatter.path}>See more...</Link>
+                        {/* <div dangerouslySetInnerHTML={{ __html: node.html }} /> */}
+                    </div>
+                ))}
+            </div>
+        </Layout>
+    );
+};
 
 export const query = graphql`
-  query {
-    allMarkdownRemark {
-      totalCount
-      edges {
-        node {
-          id
-          frontmatter {
-            title
-            date(formatString: "DD MMMM, YYYY")
-            path
-          }
-          excerpt
-          html
+    query {
+        allMarkdownRemark {
+            totalCount
+            edges {
+                node {
+                    id
+                    frontmatter {
+                        title
+                        date(formatString: "DD MMMM, YYYY")
+                        path
+                    }
+                    excerpt
+                    html
+                }
+            }
         }
-      }
     }
-  }
-`
+`;
