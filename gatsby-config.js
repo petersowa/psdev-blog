21a56ba12e262275c67f4eb9fe4cf8cfed09d2b7
petersowa <petersowa@users.noreply.github.com>
2020-01-30 20:17:15 -0600

    update manifest info

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,8 +18,8 @@ module.exports = {
         {
             resolve: `gatsby-plugin-manifest`,
             options: {
-                name: `gatsby-starter-default`,
-                short_name: `starter`,
+                name: `ps-developer-blog`,
+                short_name: `blog`,
                 start_url: `/`,
                 // background_color: `#663399`,
                 // theme_color: `#663399`,
