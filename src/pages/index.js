1afbace882e39cbec82b4df737ff494b240ba64f
petersowa <petersowa@users.noreply.github.com>
2020-01-31 21:32:58 -0600

    commented out img - unused element

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 import Layout from '../components/layout';
 import SEO from '../components/seo';
-import Img from 'gatsby-image';
+// import Img from 'gatsby-image';
 import { useStaticQuery, graphql } from 'gatsby';
 
 // import starsImage from '../images/astronomy-stars.jpg';
