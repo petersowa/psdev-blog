7402e9894c6312c9358e04c581de593d678789d1
petersowa <petersowa@users.noreply.github.com>
2020-02-05 21:45:08 -0600

    add filtering for markdown files

--- gatsby-node.js
+++ gatsby-node.js
@@ -6,37 +6,44 @@
 
 // You can delete this file if you're not using it
 
-const path = require("path")
+const path = require('path');
+
+console.log(process.env);
 
 exports.onCreateNode = ({ node }) => {
-  if (node.internal.type === "MarkdownRemark") {
-    console.log("---", node.internal.type)
-  }
-}
+    if (node.internal.type === 'MarkdownRemark') {
+        console.log('---', node.internal.type);
+    }
+};
 
 exports.createPages = async ({ actions, graphql }) => {
-  const { createPage } = actions
-  const result = await graphql(`
-    query {
-      allMarkdownRemark {
-        edges {
-          node {
-            frontmatter {
-              path
+    const { createPage } = actions;
+    const result = await graphql(`
+        query {
+            allMarkdownRemark(filter: { fileAbsolutePath: { ne: null } }) {
+                edges {
+                    node {
+                        frontmatter {
+                            path
+                        }
+                    }
+                }
             }
-          }
         }
-      }
-    }
-  `)
+    `);
 
-  result.data.allMarkdownRemark.edges.forEach(({ node }) => {
-    createPage({
-      path: node.frontmatter.path,
-      component: path.resolve("./src/templates/post-view.js"),
-      context: {
-        slug: node.frontmatter.path,
-      },
-    })
-  })
-}
+    if (result.data) {
+        result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+            console.log(JSON.stringify(node, null, 2));
+            if (node.frontmatter.path) {
+                createPage({
+                    path: node.frontmatter.path,
+                    component: path.resolve('./src/templates/post-view.js'),
+                    context: {
+                        slug: node.frontmatter.path,
+                    },
+                });
+            }
+        });
+    }
+};
