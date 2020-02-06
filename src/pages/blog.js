8fe685aa7b14a95813ec29a36a9283e79cb1edc3
petersowa <petersowa@users.noreply.github.com>
2020-02-05 21:47:50 -0600

    filter out non-filesystem markdown

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -38,7 +38,10 @@ export default ({ data }) => {
 
 export const query = graphql`
     query {
-        allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+        allMarkdownRemark(
+            sort: { fields: [frontmatter___date], order: DESC }
+            filter: { fileAbsolutePath: { ne: null } }
+        ) {
             totalCount
             edges {
                 node {
