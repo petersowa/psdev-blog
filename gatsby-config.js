e9a4ccc95d08a9d9d927347567223ae683a28946
petersowa <petersowa@users.noreply.github.com>
2020-02-06 18:45:33 -0600

    add typescript plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -60,6 +60,10 @@ module.exports = {
                 accessToken: process.env.GATSBY_CONTENTFUL_TOKEN,
             },
         },
+        {
+            resolve: `gatsby-plugin-typescript`,
+        },
+
         // this (optional) plugin enables Progressive Web App + Offline functionality
         // To learn more, visit: https://gatsby.dev/offline
         // `gatsby-plugin-offline`,
