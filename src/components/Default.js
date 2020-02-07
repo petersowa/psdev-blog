3c396eb779273655851e075ef12fb0b310d6383c
petersowa <petersowa@users.noreply.github.com>
2020-02-06 22:57:08 -0600

    add test components / redirect fails prod build

--- src/components/Default.js
+++ src/components/Default.js
@@ -1,22 +1,16 @@
 import React, { Component } from 'react';
 
-// export default class Default extends Component {
-//     state = { title: 'Sam' };
-//     constructor() {
-//         super();
-//     }
-//     componentDidMount() {
-//         fetch('https://jsonplaceholder.typicode.com/todos/1')
-//             .then(response => response.json())
-//             .then(json => {
-//                 this.setState({ title: json.title });
-//             });
-//     }
-//     render() {
-//         return <div>Hello, {this.state.title}</div>;
-//     }
-// }
+export default class Default extends Component {
+    state = { title: 'Sam' };
 
-export default function Default() {
-    return <div>test</div>;
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/todos/1')
+            .then(response => response.json())
+            .then(json => {
+                this.setState({ title: json.title });
+            });
+    }
+    render() {
+        return <div>Hello, {this.state.title}</div>;
+    }
 }
--- src/components/MainRouter.js
+++ src/components/MainRouter.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
-import { withPrefix } from 'gatsby';
-import { Router } from '@reach/router';
+// import { withPrefix } from 'gatsby';
+import { Router, Redirect } from '@reach/router';
 // import Layout from '../components/layout';
 // import Profile from "../components/Profile"
 // import Details from "../components/Details"
 // import Login from "../components/Login"
-// import Default from '../components/Default';
+import Default from './Default';
 import Random from '../modules/test';
 
 // const App = () => {
@@ -30,7 +30,11 @@ class MainRouter extends Component {
     render() {
         return (
             <Router>
+                <Default path="app" />
+                <Random path="/app/random" />
                 <Random path="/app/random2" />
+                {/* <Redirect noThrow default to="/404" /> */}
+                {/* <Random path="/app/random2" /> */}
             </Router>
         );
     }
