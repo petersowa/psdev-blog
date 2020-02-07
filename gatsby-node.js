841e667e8a386693e5f0810de21fef741562e0e4
petersowa <petersowa@users.noreply.github.com>
2020-02-06 20:06:44 -0600

    add config for dynamic routes

--- gatsby-node.js
+++ gatsby-node.js
@@ -48,14 +48,14 @@ exports.createPages = async ({ actions, graphql }) => {
 
 // Implement the Gatsby API “onCreatePage”. This is
 // called after every page is created.
-// exports.onCreatePage = async ({ page, actions }) => {
-//     const { createPage } = actions;
-//     // Only update the `/app` page.
-//     if (page.path.match(/^\/app/)) {
-//         // page.matchPath is a special key that's used for matching pages
-//         // with corresponding routes only on the client.
-//         page.matchPath = '/app/*';
-//         // Update the page.
-//         createPage(page);
-//     }
-// };
+exports.onCreatePage = async ({ page, actions }) => {
+    const { createPage } = actions;
+    // Only update the `/app` page.
+    if (page.path.match(/^\/app/)) {
+        // page.matchPath is a special key that's used for matching pages
+        // with corresponding routes only on the client.
+        page.matchPath = '/app/*';
+        // Update the page.
+        createPage(page);
+    }
+};
