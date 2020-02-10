11f528b22d9cedd9979c2c99da09652488dd5aa9
petersowa <petersowa@users.noreply.github.com>
2020-02-09 21:58:50 -0600

    code refactor into multiple components

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,10 +7,64 @@ import { useStaticQuery, graphql, Link } from 'gatsby';
 
 // import starsImage from '../images/astronomy-stars.jpg';
 
-const IndexPage = () => {
+function DelayShow({ children }) {
     const [delayShow, setShow] = React.useState(false);
+    React.useEffect(() => {
+        setTimeout(() => {
+            setShow(true);
+        }, 10);
+    }, []);
+    return (
+        <div
+            className={
+                'cover-container shadow ' + (delayShow ? 'show' : 'hide')
+            }
+        >
+            {children}
+        </div>
+    );
+}
+
+function OverflowImage({ fluid }) {
+    return (
+        <DelayShow>
+            <Img
+                className="cover-image"
+                fluid={fluid}
+                alt="milkyway on horizon"
+            ></Img>
+        </DelayShow>
+    );
+}
+
+const Post = ({ post }) => (
+    <article key={post.id} className="front-article">
+        <h1 className="front-article__title">{post.title}</h1>
+        {post.body && (
+            <div
+                className="front-article__body"
+                dangerouslySetInnerHTML={{
+                    __html: post.body.childMarkdownRemark.html,
+                }}
+            ></div>
+        )}
+        <div className="front-article__meta">
+            {/* <h6 className="front-article__meta-field">
+                                    Author: {node.author}
+                                </h6> */}
+            <h6 className="front-article__meta-field">
+                Created: {post.createdAt}
+            </h6>
+            <h6 className="front-article__meta-field">
+                Updated: {post.updatedAt}
+            </h6>
+        </div>
+    </article>
+);
+
+const IndexPage = () => {
     const data = useStaticQuery(graphql`
-        query MyQuery {
+        query myquery {
             file(relativePath: { eq: "astronomy-stars.jpg" }) {
                 id
                 childImageSharp {
@@ -19,105 +73,42 @@ const IndexPage = () => {
                     }
                 }
             }
-            markdownRemark(frontmatter: { path: { eq: "post-dev-links" } }) {
-                frontmatter {
-                    path
-                    date
-                    author
-                    title
-                }
-                html
-            }
 
-            allContentfulBlogPosts(sort: { fields: createdAt, order: ASC }) {
+            allContentfulBlogPosts(sort: { fields: [updatedAt], order: DESC }) {
                 edges {
                     node {
-                        title
-                        updatedAt(formatString: "MM/DD/YYYY H:MMa")
-                        createdAt(formatString: "MM/DD/YYYY H:MMa")
-                        author
                         id
+                        slug
                         body {
+                            body
                             childMarkdownRemark {
                                 html
-                                excerpt
                             }
                         }
-                        slug
+                        updatedAt(formatString: "MM/DD/YYYY")
+                        createdAt(formatString: "MM/DD/YYYY")
+                        title
+                        author
+                        formatType
                     }
                 }
             }
         }
     `);
 
-    React.useEffect(() => {
-        setTimeout(() => {
-            setShow(true);
-        }, 10);
-    }, []);
     return (
         <Layout>
             <SEO title="Home" />
-            <h1 className="heading">Site Info</h1>
-            <p className="article">
-                This is my new developer blog. I will have coding tips and links
-                to coding resources, videos, and great tooling
-                <Link to="/app/random2">Random Link</Link>
-            </p>
-            <div
-                className={
-                    'cover-container shadow ' + (delayShow ? 'show' : 'hide')
-                }
-            >
-                <Img
-                    className="cover-image"
-                    fluid={data.file.childImageSharp.fluid}
-                    alt="milkyway on horizon"
-                ></Img>
-            </div>
+            <h1 className="heading">Latest News</h1>
+            <OverflowImage fluid={data.file.childImageSharp.fluid} />
+
             <section>
-                {data.allContentfulBlogPosts.edges.map(({ node }) => {
-                    return (
-                        <article key={node.id} className="front-article">
-                            <h1 className="front-article__title">
-                                {node.title}
-                            </h1>
-                            <div
-                                className="front-article__body"
-                                dangerouslySetInnerHTML={{
-                                    __html: node.body.childMarkdownRemark.html,
-                                }}
-                            ></div>
-                            <div className="front-article__meta">
-                                <h6 className="front-article__meta-field">
-                                    Author: {node.author}
-                                </h6>
-                                <h6 className="front-article__meta-field">
-                                    Created: {node.createdAt}
-                                </h6>
-                                <h6 className="front-article__meta-field">
-                                    Updated: {node.updatedAt}
-                                </h6>
-                            </div>
-                        </article>
-                    );
-                })}
+                {data.allContentfulBlogPosts.edges.map(({ node }) => (
+                    <Post post={node}></Post>
+                ))}
             </section>
-            <section
-                className="post-template__content"
-                dangerouslySetInnerHTML={{
-                    __html: data.markdownRemark.html,
-                }}
-            />
-            <article className="article-columns">
-                <h1>Useful package for Gatsby and Remark</h1>
-                <p>
-                    gatsby-remark-external-links Adds the target and rel
-                    attributes to external links in markdown. This is a gatsby
-                    port of the remark-external-links remark plugin.
-                </p>
-                https://cnpmjs.org/package/gatsby-remark-external-links
-            </article>
+
+            <Link to="/app/random2">Random Link</Link>
         </Layout>
     );
 };
