30c9633df14fbc63cba4d334fed716247ed65da0
petersowa <petersowa@users.noreply.github.com>
2023-01-30 18:46:34 -0600

    update to gatsby 5.0

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 import Layout from '../components/layout';
 import SEO from '../components/seo';
-import Img from 'gatsby-image';
+import Img from 'gatsby-plugin-image';
 import { useStaticQuery, graphql, Link } from 'gatsby';
 
 import Webview from '../components/Webview';
